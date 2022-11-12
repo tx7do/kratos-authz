b37db9af417ca572c90f41117adb0fda91050df7
tx7do <yanglinbo@gmail.com>
2022-11-13 01:52:19 +0800

    feat: 重构了一下，充分利用context特性。

--- authz/options.go
+++ authz/options.go
@@ -1,6 +1,19 @@
 package authz
 
+import (
+	"context"
+	"github.com/tx7do/kratos-authz/engine"
+)
+
 type Option func(*options)
 
 type options struct {
+}
+
+func NewContext(ctx context.Context, claims *engine.AuthClaims) context.Context {
+	return engine.ContextWithAuthClaims(ctx, claims)
+}
+
+func FromContext(ctx context.Context) (*engine.AuthClaims, bool) {
+	return engine.AuthClaimsFromContext(ctx)
 }

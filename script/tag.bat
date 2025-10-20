dfee683360dad5e40f707039a32354f775c75c6e
Bobo <yanglinbo@gmail.com>
2025-10-20 21:51:05 +0800

    feat: go.mod upgrade

--- script/tag.bat
+++ script/tag.bat
@@ -1,9 +1,9 @@
 git tag v1.1.6
 
-git tag engine/casbin/v1.1.9
-git tag engine/opa/v1.1.12
+git tag engine/casbin/v1.1.10
+git tag engine/opa/v1.1.13
 git tag engine/zanzibar/v1.1.7
 
-git tag middleware/v1.1.9
+git tag middleware/v1.1.10
 
 git push origin --tags
